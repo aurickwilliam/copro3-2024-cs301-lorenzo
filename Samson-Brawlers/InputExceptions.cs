3fe67d5c192f92b295007c9075f6a7c45f20c549
aurickwilliam <aurickwilliam21@gmail.com>
2024-12-02 13:03:58 +0800

    Finish Character Customization Input

--- Samson-Brawlers/InputExceptions.cs
+++ Samson-Brawlers/InputExceptions.cs
@@ -4,9 +4,17 @@ namespace Samson_Brawlers
 {
     public class InputNotAChoiceException : Exception 
     {
-        public InputNotAChoiceException(string message = "PLEASE SELECT FROM THE GIVEN CHOICES!") : base(message) 
+        public InputNotAChoiceException(string message = "\n\t\t\t PLEASE SELECT FROM THE GIVEN CHOICES!\n") : base(message) 
         {
             
+        }
+    }
+
+    public class InvalidCharacterException : Exception
+    {
+        public InvalidCharacterException(string message = "\n\t\t\t INVALID CHARACTER!\n") : base(message)
+        {
+
         }
     }
 }
--- Samson-Brawlers/MainClass.cs
+++ Samson-Brawlers/MainClass.cs
@@ -4,6 +4,8 @@ namespace Samson_Brawlers
 {
     public class MainClass
     {
+        private List<Character> characters = new List<Character>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("+=======================================================================================================+");
@@ -51,12 +53,148 @@ namespace Samson_Brawlers
                     main.CreateCharacter(inputHandler);
 
                     break;
+                case "2":
+                    break;
             }
         }
 
         public void CreateCharacter(InputHandler inputHandler)
         {
-            
+            // PHYSICAL APPEARANCE
+            Console.WriteLine("\t\t\t+------------------------------------------------------+");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+            Console.WriteLine("\t\t\t|\t\t   PHYSICAL APPEARANCE\t\t       |");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+
+            string gender = inputHandler.GenderInput();
+            string bodyType = inputHandler.BodyTypeInput();
+            string height = inputHandler.HeightInput();
+            string skinColor = inputHandler.SkinColorInput();
+            bool skinWrinkles = inputHandler.SkinWrinklesInput();
+            string hairStyle = inputHandler.HairStyleInput();
+            string hairColor = inputHandler.HairColorInput();
+            string headShape = inputHandler.HeadShapeInput();
+            string eyeShape = inputHandler.EyeShapeInput();
+            string eyeColor = inputHandler.EyeColorInput();
+            string facialHairStyle = inputHandler.FacialHairStyleInput();
+
+            Console.WriteLine("\t\t\t+------------------------------------------------------+");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+            Console.WriteLine("\t\t\t|\t\t CLOTHING & ACCESSORIES\t       \t       |");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+
+            string outfitSet = inputHandler.OutfitSetInput();
+            string topClothing = inputHandler.TopClothingInput();
+            string bottomClothing = inputHandler.BottomClothingInput();
+            string footWear = inputHandler.FootWearInput();
+            string accessories = inputHandler.AccessoriesInput();
+            bool tattoo = inputHandler.TattooInput();
+            bool scar = inputHandler.ScarInput();
+            bool aura = inputHandler.AuraInput();
+
+            Console.WriteLine("\t\t\t+------------------------------------------------------+");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+            Console.WriteLine("\t\t\t|\t\t   WEAPONS & ATTACKS\t\t       |");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+
+            string meleeWeapon = inputHandler.MeleeWeaponInput();
+            string fightingStyle = inputHandler.FightingStyleInput();
+            string stance = inputHandler.StanceInput();
+
+            List<int> stats = inputHandler.StatisticsInput();
+
+            Console.WriteLine("\t\t\t+------------------------------------------------------+");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+            Console.WriteLine("\t\t\t|\t\t CHARACTERS BIOGRAPHY\t\t       |");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+            Console.WriteLine("\t\t\t+------------------------------------------------------+\n");
+
+            string name = inputHandler.NameInput();
+            Console.WriteLine("\t\t\t+------------------------------------------------------+\n");
+            string title = inputHandler.TitleInput();
+            Console.WriteLine("\t\t\t+------------------------------------------------------+\n");
+
+            Console.WriteLine("\t\t\t+------------------------------------------------------+");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+            Console.WriteLine("\t\t\t|\t     CHARACTER CREATED SUCCESSFULLY!\t       |");
+            Console.WriteLine("\t\t\t|\t\t\t\t\t\t       |");
+            Console.WriteLine("\t\t\t+------------------------------------------------------+");
+
+            Console.WriteLine("\n\n\nData:");
+            Console.WriteLine("\nPHYSICAL APPEARANCE");
+            Console.WriteLine("GENDER: " + gender);
+            Console.WriteLine("BODY TYPE: " + bodyType);
+            Console.WriteLine("HEIGHT: " + height);
+            Console.WriteLine("SKIN COLOR: " + skinColor);
+            Console.WriteLine("SKIN WRINKLES: " + skinWrinkles);
+            Console.WriteLine("HAIR STYLE: " + hairStyle);
+            Console.WriteLine("HAIR COLOR: " + hairColor);
+            Console.WriteLine("HEAD SHAPE: " + headShape);
+            Console.WriteLine("EYE SHAPE: " + eyeShape);
+            Console.WriteLine("EYE COLOR: " + eyeColor);
+            Console.WriteLine("FACIAL HAIR STYLE: " + facialHairStyle);
+
+            Console.WriteLine("\nCLOTHING & ACCESSORIES");
+            Console.WriteLine("OUTFIT SET: " + outfitSet);
+            Console.WriteLine("TOP CLOTHING: " + topClothing);
+            Console.WriteLine("BOTTOM CLOTHING: " + bottomClothing);
+            Console.WriteLine("FOOTWEAR: " + footWear);
+            Console.WriteLine("ACCESSORIES: " + accessories);
+            Console.WriteLine("TATTOO: " + tattoo);
+            Console.WriteLine("SCAR: " + scar);
+            Console.WriteLine("AURA: " + aura);
+
+            Console.WriteLine("\nWEAPONS & ATTACKS");
+            Console.WriteLine("MELEE WEAPON: " + meleeWeapon);
+            Console.WriteLine("FIGHTING STYLE: " + fightingStyle);
+            Console.WriteLine("STANCE: " + stance);
+
+            Console.WriteLine("\nSTATISTICS");
+            Console.WriteLine("ATTACK POWER: " + stats[0]);    
+            Console.WriteLine("DEFENSE: " + stats[1]);    
+            Console.WriteLine("SPEED: " + stats[2]);    
+            Console.WriteLine("STAMINA: " + stats[3]);    
+            Console.WriteLine("HEALTH: " + stats[4]);    
+            Console.WriteLine("SPECIAL METER GAIN: " + stats[5]);
+
+            Console.WriteLine("\nBIOGRAPHY");
+            Console.WriteLine("NAME: " + name);
+            Console.WriteLine("TITLE: " + title);
+
+            Character character = new Character(
+                    name,
+                    title,
+                    gender,
+                    bodyType,
+                    height,
+                    skinColor,
+                    hairStyle,
+                    hairColor,
+                    headShape,
+                    eyeShape,
+                    eyeColor,
+                    facialHairStyle,
+                    outfitSet,
+                    topClothing,
+                    bottomClothing,
+                    footWear,
+                    accessories,
+                    meleeWeapon,
+                    fightingStyle,
+                    stance,
+                    skinWrinkles,
+                    tattoo,
+                    scar,
+                    aura,
+                    stats[0],
+                    stats[1],
+                    stats[2],
+                    stats[3],
+                    stats[4],
+                    stats[5]
+                );
+
+            characters.Add( character );
         }
     }
 }
